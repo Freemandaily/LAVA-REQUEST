a6fd77afde0089409d14046671e396ca3af8dbba
Freeman <107298762+Freemandaily@users.noreply.github.com>
2024-04-21 13:08:57 +0100

    Update lava_divider.py

--- lava_divider.py
+++ lava_divider.py
@@ -6,8 +6,9 @@ import subprocess
 
 start = True
 while start:
-    for number in range(5):
+    for number in range(7):
         subprocess.Popen(['python3','main.py',f'LAVA_{number}'])
         time.sleep(2)
-    print('WAITING FOR THE NEXT 30 MINUTE BEFORE SENDING AGAIN')
-    time.sleep(1800)
+    print('WAITING FOR THE NEXT 20 MINUTE BEFORE SENDING AGAIN')
+    sys.exit()
+    #time.sleep(1800)
